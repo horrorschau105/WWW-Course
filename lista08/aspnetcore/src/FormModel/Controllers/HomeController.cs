40552c2448cd7d7cea5d8cc282829d7869af846f
horrorschau105 <piotrekgdowski@wp.pl>
2017-01-10 22:31:35 +0100

    zad 1 in 'form model', zad 2 in 'mvc example'

--- lista08/aspnetcore/src/FormModel/Controllers/HomeController.cs
+++ lista08/aspnetcore/src/FormModel/Controllers/HomeController.cs
@@ -11,10 +11,40 @@ namespace FormModel.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
+            ViewBag.Title = "IndexTitle";
             return View();
         }
+        [Route("person")]
+        public IActionResult Person()
+        {
+            ViewBag.Title = "Person";
+            return Person();
+        }
+
+        [Route("string")]
+        public IActionResult String()
+        {
+            ViewBag.Title = "StringTitle";
+            return Content("String");
+        }
+
+        [Route("picture")]
+        public IActionResult Picture()
+        {
+            ViewBag.Title = "Picture";
+            return File("picture.jpg", "image/jpg");
+        }
+
+        [Route("json")]
+        public IActionResult Json()
+        {
+            ViewBag.Title = "Json";
+            return Json(new char[5] { 'a', 'b', 'c', 'd', 'e' });
+        }
+
+        
 
         [HttpPost]
         public ActionResult Index(Person person)
